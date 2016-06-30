6eb8df63e7f16b7c8567ae570e2b287a64de5784
Yazid Ismail <flight846@gmail.com>
2016-06-30 19:25:12 +0000

    Edit destroy method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,7 +24,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to user_posts_url(current_user), notice: 'Comment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
